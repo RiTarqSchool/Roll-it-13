0cb6155cb2ba660468dfe0196d694baf95e66117
Nua-Tarq <163465539+RiTarqSchool@users.noreply.github.com>
2025-02-14 10:12:02 +1300

    yes/no checker (not complete)

---
+++ yes_no_basic.py
@@ -0,0 +1,7 @@
+want_instructiongs =input("Do you want to see the instructions? ").lower()
+
+# check if the user says yes or no
+if want_instructiongs == "yes" or want_instructiongs == "y":
+    print("you said yes")
+elif want_instructiongs == "no" or want_instructiongs== "n":
+    print("you said no")
